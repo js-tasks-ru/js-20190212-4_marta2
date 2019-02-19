b137b3680caf92e46911125ec4357beb8ff3777e
Marta Rudkouskaya <stdsparrow@gmail.com>
2019-02-20 01:18:39 +0300

    Result for 1.3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,33 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
 
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function getMinMax(string) {
+    
+    let arr = string.split("");
+    let arrNumeric = [];
+    let obj = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        let tmp = [];
+        while (arr[i] && isNumeric(arr[i]) || arr[i] === '.' || arr[i] === '-') {
+            tmp.push(arr[i]);
+            i++;
+        }
+        if(tmp.join('') !== '-' && tmp[0]) arrNumeric.push(tmp.join(''));
+    }
+
+    for(let i= 0; i < arrNumeric.length; i++){
+        if (!obj.min || +obj.min > +arrNumeric[i]) {
+            obj.min = arrNumeric[i];
+        } else if (!obj.max || +obj.max < +arrNumeric[i]) {
+            obj.max = arrNumeric[i];
+        }
+    }
+    return obj;
 }
 
