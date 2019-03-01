9ebf0b7ba14e6ea81ccac501b0b72967f1151c69
MartaRudkouskaya <47795012+MartaRudkouskaya@users.noreply.github.com>
2019-03-01 19:41:32 +0300

    Update index.js

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -5,5 +5,15 @@
  * @param {Friend[]} friends
  * @return {HTMLUListElement}
  */
-function makeFriendsList (friends) {
+
+function clone (obj) {
+    let newObj = {};
+    for(let key in obj){
+        if(typeof(obj[key]) === 'object' && obj[key] !== null) {
+            newObj[key] = clone(obj[key]);
+            continue;
+        }
+        newObj[key] = obj[key];
+    }
+    return newObj;
 }
