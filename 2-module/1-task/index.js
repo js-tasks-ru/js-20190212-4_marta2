dac15fda5b1480625e5b2392d8eeba774ce55add
MartaRudkouskaya <47795012+MartaRudkouskaya@users.noreply.github.com>
2019-03-01 19:42:39 +0300

    Update index.js

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,7 +1,19 @@
+'use strict';
+
 /**
  * Клонируем объект
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
+
 function clone (obj) {
+    let newObj = {};
+    for(let key in obj){
+        if(typeof(obj[key]) === 'object' && obj[key] !== null) {
+            newObj[key] = clone(obj[key]);
+            continue;
+        }
+        newObj[key] = obj[key];
+    }
+    return newObj;
 }
