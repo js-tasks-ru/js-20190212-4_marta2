543380b050dcb2b32a1113f1f1d12218c55cd857
Marta Rudkouskaya <stdsparrow@gmail.com>
2019-03-03 19:03:31 +0300

    Result for 3.1

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,6 +5,11 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+    let str = '';
+    for (let i = 0; i < data.length; i++){
+        if(data[i].age <= age){
+            str = str + data[i].name + ', ' + data[i].balance + '\n';
+        }
+    }
+    return str;
 }
-
-
