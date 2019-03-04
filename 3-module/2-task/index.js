a67cac4a3fbae2f3b0bd8f6df98972fb296e022e
Marta Rudkouskaya <stdsparrow@gmail.com>
2019-03-04 13:31:38 +0300

    Result for 3.2

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,6 +4,38 @@ let calendar = {
 };
 
 calendar[Symbol.iterator] = function () {
+    let current = this.from;
+    let last = this.to.setDate(this.to.getDate() + 1);
+
+    return {
+        next() {
+            if (current <= last) {
+                let day = current.getDay();
+                let strDate = '';
+                if (day === 0 || day === 6) {
+                    if (current.getDate() < 10) {
+                        strDate = "[0" + current.getDate() + "]";
+                    } else {
+                        strDate = "[" + current.getDate() + "]";
+                    }
+                } else {
+                    if (current.getDate() < 10) {
+                        strDate = "0" + current.getDate();
+                    } else {
+                        strDate = current.getDate();
+                    }                    
+                }
+                current.setDate(current.getDate() + 1);
+                return {
+                    done: false,
+                    value: strDate
+                };
+            } else {
+                return {
+                    done: true
+                };
+            }
+        }
+
+    }
 };
-
-
