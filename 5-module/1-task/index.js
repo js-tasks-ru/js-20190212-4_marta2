7d37770fb4153047cb74a2d82b22e1b83db0afa4
MartaRudkouskaya <47795012+MartaRudkouskaya@users.noreply.github.com>
2019-03-11 14:49:32 +0300

    Result for 5.1

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -4,5 +4,26 @@
  * Метод устанавливает необходимые по условию аттрибуты таблице
  * @param {Element} table
  */
+
 function highlight(table) {
+    let trs = table.querySelectorAll('tbody > tr');
+    for(let tr of trs){
+        if(tr.lastElementChild.dataset.available === 'true'){
+            tr.classList.add('available');
+        } else if(!tr.lastElementChild.dataset.available) {
+            tr.hidden = true;
+        } else {
+            tr.classList.add('unavailable');
+        }
+
+        if(tr.children[2].textContent === 'm'){
+            tr.classList.add('male');
+        } else if(tr.children[2].textContent === 'f'){
+            tr.classList.add('female');
+        }
+
+        if(tr.children[1].textContent < 18){
+            tr.style.textDecoration = 'line-through';
+        }
+    }
 }
